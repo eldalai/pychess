52fb9cc20662f35d3d31e49d37369ff0c05d350e
Gabriel Flores <gabrielf@eventbrite.com>
2016-06-24 14:07:57 -0300

    pip setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+import os
+from setuptools import setup
+
+setup(
+    name = "Python Chess",
+    version = "0.0.1",
+    author = "Gabriel Flores",
+    author_email = "eldalai@gmail.com",
+    description = ("Python pure chess logic"),
+)
