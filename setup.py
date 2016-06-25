3e4bfadf56a46c522ccfd9e191bac3b967ead4bc
Gabriel Flores <gabrielf@eventbrite.com>
2016-06-25 11:46:29 -0300

    Bump version: 0.0.1 → 0.1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="Python Chess",
-    version="0.0.1",
+    version="0.1.0",
     author="Gabriel Flores",
     author_email="eldalai@gmail.com",
     description=("Python pure chess logic"),
