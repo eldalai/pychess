7588a0834713c136f09902dd65dcd3f45e5fb5c6
Gabriel Flores <gabrielf@eventbrite.com>
2016-06-25 11:46:26 -0300

    bumpversion

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name = "Python Chess",
-    version = "0.0.1",
-    author = "Gabriel Flores",
-    author_email = "eldalai@gmail.com",
-    description = ("Python pure chess logic"),
+    name="Python Chess",
+    version="0.0.1",
+    author="Gabriel Flores",
+    author_email="eldalai@gmail.com",
+    description=("Python pure chess logic"),
     package=find_packages(),
 )
